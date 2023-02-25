e2462a83fa84c5e2850569fd1893664d5b72d10c
Lilliana <68789766+Paisseon@users.noreply.github.com>
2023-02-26 00:40:42 +0100

    Fix extraction
    No longer prints the errors, because it wouldn’t be used anyway. Might fix it better later idk
    
    Signed-off-by: Lilliana <68789766+Paisseon@users.noreply.github.com>

--- Sources/AzulaKit/Extensions/Data.swift
+++ Sources/AzulaKit/Extensions/Data.swift
@@ -12,7 +12,6 @@ extension Data {
         at offset: Int = 0
     ) -> T {
         guard offset + MemoryLayout<T>.size < count else {
-            RainbowLogger.log(String(format: "Offset 0x%X is out of bounds", offset + MemoryLayout<T>.size), type: .error)
             exit(1)
         }
         
@@ -20,7 +19,6 @@ extension Data {
         
         return data.withUnsafeBytes { dataBytes in
             guard let pointer: UnsafePointer<T> = dataBytes.baseAddress?.bindMemory(to: T.self, capacity: 1) else {
-                RainbowLogger.log(String(format: "Couldn't find pointer for \(T.self) at 0x%X", offset), type: .error)
                 exit(1)
             }
             
