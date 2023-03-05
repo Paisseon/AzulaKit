a13c8399459cd37846a3a7c263d9bdf2fd904c50
Lilliana <68789766+Paisseon@users.noreply.github.com>
2023-03-05 16:03:05 +0100

    Make Log.init() public
    Signed-off-by: Lilliana <68789766+Paisseon@users.noreply.github.com>

--- Sources/AzulaKit/Types/Logging/Log.swift
+++ Sources/AzulaKit/Types/Logging/Log.swift
@@ -5,7 +5,15 @@
 //  Created by Lilliana on 05/03/2023.
 //
 
-public struct Log {
-    let text: String
-    let type: PrintType
+public struct Log: Hashable {
+    public let text: String
+    public let type: PrintType
+    
+    public init(
+        text: String,
+        type: PrintType
+    ) {
+        self.text = text
+        self.type = type
+    }
 }
